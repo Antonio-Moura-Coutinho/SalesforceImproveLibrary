4a8d271679af9b44c7c6cefcffdaf03d3ae3a62e
Antonio-Moura-Coutinho <tonitrigueiros@gmail.com>
2024-10-18 02:43:37 +0800

    hiphen

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="salesforce-improve",  # Nome do pacote
-    version="0.2",
+    name="salesforce_improve",  # Nome do pacote
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[
         "simple-salesforce",  # Inclua todas as dependências necessárias
