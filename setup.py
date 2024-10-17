5d6aff6c5e25d39be34811e82b5eea190294ba59
Antonio-Moura-Coutinho <63213425+Antonio-Moura-Coutinho@users.noreply.github.com>
2024-10-18 02:25:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="salesforce-improve",  # Nome do pacote
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         "simple-salesforce",  # Inclua todas as dependências necessárias
